5ca1a86f21f75c0b7843ef4e76d7b850542cfb04
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2023-06-21 18:11:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Roquet SmoothieBreakfast Menu')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Advocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
 
